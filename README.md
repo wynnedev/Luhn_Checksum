8bd6bdac601cd0715d1389bd6079c0ba684981b4
wynneN <wjn17@my.fsu.edu>
2018-11-10 15:37:58 -0500

    Added function to double values per luhn checksum completed and tested program

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Luhn_Checksum
 A small program to determine if a sequence valid using luhn checksum as a standard
+
+1.  get user input for number characters
+2.  keep running total of value
+3.  double value of numeric character for every other character
+        a. if double value if greater than 10 add digits together
+        b. add value to running total
+
+4.  divide running total by 10 to determine if valid
+
--- main.cpp
+++ main.cpp
@@ -4,12 +4,61 @@
  * Description: The program will take an undetermined number of integers and determine if they have a valid
  * checksum using Luhn Checksum standard.  If the checksum is invalid the program will give the next needed
  * integer to make the system valid.
+ *
+ * Constraints:  You may not store the numbers entered.  Each value must be processed one at a time.
  */
 
 #include <iostream>
 
+#define EOL 10      //end of line character
+
+int double_digit(char);
 int main() {
+
+    int even_total{0};
+    int odd_total{0};
+    int counter{0};
+    char user_entry;
+
     std::cout << "***Luhn Checksum***" << std::endl;
     std::cout << "Enter Valid Sequence: " <<std::endl;
+    user_entry = std::cin.get();
+
+    while(user_entry!= EOL){
+
+        if(++counter % 2){
+            odd_total += int(user_entry) - 48;
+            even_total += double_digit(user_entry);
+        }
+        else{
+            even_total += int(user_entry) - 48;
+            odd_total += double_digit(user_entry);
+        }
+        user_entry = std::cin.get();
+    }
+
+    if(counter%2){
+        if(odd_total % 10)
+            std::cout << "Checksum Invalid";
+        else
+            std::cout << "Checksum Valid";
+    }
+
+    else {
+        if (even_total % 10)
+            std::cout << "Checksum Invalid ";
+        else
+            std::cout << "Checksum Valid";
+    }
+
     return 0;
+}
+
+int double_digit(char c){
+    int num = (int(c) - 48) * 2;
+
+    if(num >= 10)
+        return 1 + num%10;
+    else
+        return num;
 }
