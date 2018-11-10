5a8163e62469015f0738738771d8a40d052496e3
wynneN <wjn17@my.fsu.edu>
2018-11-10 14:54:48 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Project Name: Luhn Checksum
+ *
+ * Description: The program will take an undetermined number of integers and determine if they have a valid
+ * checksum using Luhn Checksum standard.  If the checksum is invalid the program will give the next needed
+ * integer to make the system valid.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "***Luhn Checksum***" << std::endl;
+    std::cout << "Enter Valid Sequence: " <<std::endl;
+    return 0;
+}
